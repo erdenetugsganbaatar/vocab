56a1de0ac8c8f3ac327b4d2213d2999fa51fda9f
erdenetugs ganbaatar <erdenetugsganbaatar@gmail.com>
2024-05-29 21:56:16 +0800

    feat: add useResolver hof

--- back/src/graphql/resolvers/queries/vocab/listVocabs.ts
+++ back/src/graphql/resolvers/queries/vocab/listVocabs.ts
@@ -1,23 +1,22 @@
 import { QueryResolvers } from "@/generated/graphql";
 import { useUser } from "@/helpers/scope";
 import { prisma } from "@/lib/prisma";
+import { useResolver } from "@/lib/resolver";
 
-export const listVocabs: QueryResolvers["listVocabs"] = async (
-  _parent,
-  _args,
-  contextValue
-) => {
-  const user = await useUser(contextValue.user);
+export const listVocabs: QueryResolvers["listVocabs"] = useResolver(
+  async ({ context }) => {
+    const user = await useUser(context.user);
 
-  try {
-    return await prisma.vocab.findMany({
-      where: {
-        userId: user.id,
-      },
-    });
-  } catch (e) {
-    console.error(e);
-    // TODO: handle error
+    try {
+      return await prisma.vocab.findMany({
+        where: {
+          userId: user.id,
+        },
+      });
+    } catch (e) {
+      console.error(e);
+      // TODO: handle error
+    }
+    return null;
   }
-  return null;
-};
+);
